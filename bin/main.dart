fb362443c84d32f2f7fec5590112150abc9f42d6
esimed <contact@esimed.fr>
2023-06-06 15:14:27 +0200

    first commit

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:template_dart_console/example_tp/example.dart' as example;
+
+void main(List<String> arguments) {
+  print(example.sayHello());
+}
---
+++ test/example_test.dart
@@ -0,0 +1,8 @@
+import 'package:template_dart_console/example_tp/example.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('example', () {
+    expect(sayHello(), "Bonjour");
+  });
+}
