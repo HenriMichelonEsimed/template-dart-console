9b1a56048633353393618da38e1e1131873da03a
esimed <contact@esimed.fr>
2023-06-06 16:33:35 +0200

    Project default name

--- bin/main.dart
+++ bin/main.dart
@@ -1,4 +1,4 @@
-import 'package:template_dart_console/example_tp/example.dart' as example;
+import 'package:example/example_tp/example.dart' as example;
 
 void main(List<String> arguments) {
   print(example.sayHello());
--- test/example_test.dart
+++ test/example_test.dart
@@ -1,4 +1,4 @@
-import 'package:template_dart_console/example_tp/example.dart';
+import 'package:example/example_tp/example.dart';
 import 'package:test/test.dart';
 
 void main() {
